e7287a228e00fb8a3b84515668ede6a2427773ae
Никита Фомин <n.f.@MacBook-Air-Nikita.local>
2025-03-17 20:42:17 +0700

    Обновил файлы к 3 заданию

---
+++ 3/app3.js
@@ -0,0 +1,6 @@
+const titleProject = "Оценка стоимости";
+const screensValue = "Шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = "Цена реализации одного экрана";
+const percentage = 10;
+const fullPrice = 1500;
+const responsive = 22;
